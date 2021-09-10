9253d9ed5bfc4a6e9d35ba4527a06ab3c786aa3a
neils-realtair <58451929+neil-realtair@users.noreply.github.com>
2021-09-10 17:59:29 +0800

    Update Console message

--- DemoDataStruct/DemoDataStruct/Program.cs
+++ DemoDataStruct/DemoDataStruct/Program.cs
@@ -6,7 +6,7 @@ namespace DemoDataStruct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello ACLC BSCS - Algorithm!");
             Console.ReadKey();
         }
     }
